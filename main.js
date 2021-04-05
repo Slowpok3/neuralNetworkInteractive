aa64ba8dca638953a80a7a04c765080b82a27e85
Abrar Zaman <abrarzaman2003@gmail.com>
2021-04-05 18:45:26 -0500

    started working on drawing

--- main.js
+++ main.js
@@ -24,7 +24,7 @@ const button1 = inputButton.addEventListener('click', function (){
     const nodeButton = document.createElement("button");
     nodeButton.type = "submit";
     nodeButton.setAttribute("id", "nodeButton");
-    nodeButton.innerText = "yew";
+    nodeButton.innerText = "Confirm Number of Nodes in Each Layer";
 
     parent.appendChild(bigLabel);
     parent.appendChild(nodeButton);
@@ -46,43 +46,58 @@ const button1 = inputButton.addEventListener('click', function (){
     }
 
     nodeInputWindow.appendChild(parent);
-
-
-
-
+    const nodes = new Array(layers);
+    const displayParent = document.createElement('div');
+    parent.appendChild(displayParent);
+    displayParent.appendChild(document.createElement('h1'));
     
-
     nodeButton.addEventListener('click',function(){
-        const nodes = new Array(layers);
+        displayParent.removeChild(displayParent.firstChild);
 
-    for(i = 1; i<=layers; i++){
-        const x = document.getElementById("layer"+i+"");
-        nodes[i-1] = (x.value);
-    }
+        for(i = 1; i<=layers; i++){
+            const x = document.getElementById("layer"+i+"");
+            nodes[i-1] = (x.value);
+        }
+        console.log(nodes);
 
-    console.log(nodes);
+        const weightsDisplay = document.createElement('h1');
+        var weight = 1;
+        for (i = 0; i<nodes.length; i++){
+            weight*= nodes[i];
+        }
+        weightsDisplay.innerText = "Number of weights: " + weight;
+
+        displayParent.appendChild(weightsDisplay);
+        
+     
+        
+
+
+        
+
     })
 
     
 });
 
 
-
-
-
-
-
-
-// function setup() {
-//     const myCanvas = createCanvas(1000, 1000);
-//     myCanvas.parent("content");
-// }
+function setup() {
+    const myCanvas = createCanvas(1000, 1000);
+    myCanvas.parent("content");
+    background(220);
+}
   
-// function draw() {
-//     if (mouseIsPressed) {
-//       fill(0);
-//     } else {
-//       fill(255);
-//     }
-//     ellipse(mouseX, mouseY, 80, 80);
-// }
+function draw() {
+    
+    if (mouseIsPressed) {
+      fill(0);
+    } else {
+      fill(255);
+    }
+    ellipse(mouseX, mouseY, 80, 80);
+}
+
+
+
+
+
